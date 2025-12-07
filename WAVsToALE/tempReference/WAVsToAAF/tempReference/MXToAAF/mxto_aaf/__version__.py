41658c84390f7586982900d08a605852d61de1b8
Jason Brodkey <jasonbrodkey@Mac.attlocal.net>
2025-12-07 08:45:26 -0800

    Refactor: flatten repository structure (move WAVsToALE/ contents to root)

--- WAVsToALE/tempReference/WAVsToAAF/tempReference/MXToAAF/mxto_aaf/__version__.py
+++
@@ -1,5 +0,0 @@
-"""Version information for MXToAAF"""
-
-__version__ = "0.9.0"
-__author__ = "Jason Brodkey"
-__description__ = "Convert music files to embedded AAFs for Avid with metadata extraction"
